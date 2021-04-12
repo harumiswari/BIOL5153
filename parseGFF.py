09c79ea0f8b84304aad2f0e767cbf60c002bf19b
Harumi Padmaswari <mhpadmas@uark.edu>
2021-04-12 08:21:24 -0500

    Adding new version of the script

--- parseGFF.py
+++ parseGFF.py
@@ -2,7 +2,7 @@
 
 import csv
 import argparse
-# from Bio import SeqIO
+from Bio import SeqIO
 
 # this script will parse a GFF file and extract each feature from the genome
 # inputs: 1) GFF file, 2) corresponding genome sequence (FASTA format)
@@ -18,13 +18,8 @@ parser.add_argument('fasta', help ='name of the FASTA file')
 args = parser.parse_args()
 # print(args.gff)
 
-'''
-# read in GFF file
-gff_input = 'c:/Users/harum/OneDrive/Documents/scripts/watermelon.gff'
-
-# fasta filename
-fasta_input = 'c:/Users/harum/OneDrive/Documents/scripts/watermelon.fsa'
-'''
+# open and read in the FASTA file
+gene = SeqIO.read(args.fasta, 'fasta')
 
 # open and read in FASTA file (with open(gff_input,'r') as gff_in:)
 with open(args.gff,'r') as gff_in:
@@ -32,23 +27,24 @@ with open(args.gff,'r') as gff_in:
     reader = csv.reader(gff_in, delimiter='\t')
     # loop over all the lines in our reader object of the parse file
     for line in reader:
-        print(line)
         start = line[3]
         end = line[4]
         strand = line[6]
-        print(start)
-'''
-genome = seqIO.read(args.fasta,'fasta')
-print(genome)
-print(genome.id)
-print(len(genome.seq))
-'''
+        
+        # print header
+        print('>' + gene.id, line[-1])
 
-# unused code but saved as a memory on how to split the line
-'''
-    for l in gff_in:
-        columns = l.split('\t')
-        print(columns[3], columns[4])
-'''
+        # check in reverse complement
+        if strand == '-':
+            anti_seq = gene.seq[start-1:end].reverse_complement()
+            print(anti_seq)
+        # print the sequence of sense strand
+        else:
+            sense_seq = gene.seq[start[-1:end]]
+            print(sense_seq)
+        
+        print()
+
+
 
 
