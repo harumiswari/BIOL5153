0f9f37a89d31bc28a36be1ebd44a340dd2a29a40
Harumi Padmaswari <mhpadmas@uark.edu>
2021-04-21 21:22:13 -0500

    assn07

--- parseGFF.py
+++ parseGFF.py
@@ -19,21 +19,33 @@ args = parser.parse_args()
 # print(args.gff)
 
 # open and read in the FASTA file
-gene = SeqIO.read(args.fasta, 'fasta')
+gene.seq = SeqIO.read(args.fasta, 'fasta')
 
+# function for reverse complements:
+def rev_comp(rev_gene):
+    return rev_gene.reverse_complement()
+
+whole_exons = ''
+files = 'exon' + ''.fasta'
+outfile = open(files, 'w')
 # open and read in FASTA file (with open(gff_input,'r') as gff_in:)
 with open(args.gff,'r') as gff_in:
     # create a csv reader object
     reader = csv.reader(gff_in, delimiter='\t')
     # loop over all the lines in our reader object of the parse file
     for line in reader:
-        start = line[3]
-        end = line[4]
-        strand = line[6]
-        
+        if 'exon' in reader[-1]:
+            start = int(line[3])
+            end = int(line[4])
+            exons = str(gene.seq[start:end])
+            whole_exons+=exons
+       
         # print header
         print('>' + gene.id, line[-1])
+        print(whole_exons)
 
+outfile.close()
+'''
         # check in reverse complement
         if strand == '-':
             anti_seq = gene.seq[start-1:end].reverse_complement()
@@ -44,7 +56,7 @@ with open(args.gff,'r') as gff_in:
             print(sense_seq)
         
         print()
-
+'''
 
 
 
