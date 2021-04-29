3f91e0efd8c9343d362a221d094968d6aae6bfee
Harumi Padmaswari <mhpadmas@uark.edu>
2021-04-28 22:01:25 -0500

    new version of the answer

--- parseGFF.py
+++ parseGFF.py
@@ -1,11 +1,22 @@
 #! /usr/bin/env python3
 
 import csv
+import re
 import argparse
 from Bio import SeqIO
+from collections import defaultdict
 
 # this script will parse a GFF file and extract each feature from the genome
 # inputs: 1) GFF file, 2) corresponding genome sequence (FASTA format)
+
+def get_seq(s, e, str):
+    # rxtract the sequence
+    fragment = gene.seq[int(s):int(e)]
+    #check for + or - strand and returns as is for reverse complement
+    if(str == '+'):
+        return fragment
+    else:
+        return fragment.reverse_complement()
 
 # create an argument parser object
 parser = argparse.ArgumentParser(description = 'this script will parse a GFF file and extract each feature from the genome')
@@ -21,30 +32,66 @@ args = parser.parse_args()
 # open and read in the FASTA file
 gene.seq = SeqIO.read(args.fasta, 'fasta')
 
-# function for reverse complements:
-def rev_comp(rev_gene):
-    return rev_gene.reverse_complement()
+# create dictionoary for genes with intron/multiple exons
+# key = gene name, value = list of exon sequences
+exons_dict = defaultdict(dict)
 
-whole_exons = ''
-files = 'exon' + ''.fasta'
-outfile = open(files, 'w')
 # open and read in FASTA file (with open(gff_input,'r') as gff_in:)
 with open(args.gff,'r') as gff_in:
     # create a csv reader object
     reader = csv.reader(gff_in, delimiter='\t')
     # loop over all the lines in our reader object of the parse file
     for line in reader:
-        if 'exon' in reader[-1]:
-            start = int(line[3])
-            end = int(line[4])
-            exons = str(gene.seq[start:end])
-            whole_exons+=exons
-       
-        # print header
-        print('>' + gene.id, line[-1])
-        print(whole_exons)
+        species         = line[0].replace(' ','_')
+        feature_type    = line[2]
+        start           = line[3]
+        end             = line[4]
+        strand          = line [6]
 
-outfile.close()
+        #split the attribute fields in a list
+        attr_fields = line[8].split()
+        gene_name = attr_fields[1]
+
+        if(feature_type == 'CDS'):
+
+            # search for exon annotation
+            match = re.search("exon\s+(\d)", line[8])
+            # test for whether there are multiple exons
+            if(match):
+                #create fasta header which allso nbe our dictionary key for exons
+                header              = species + "_" + gene_name
+                #get the exon number
+                exon_number         = (match.group(1))
+                #get the sequence for this exon
+                exon                = get_seq(int)start)-1, int(end), strand)
+                
+                if(header in exons_dict):
+                    exons_dict[header][exon_number] = exon
+                
+                else:
+                    exons_dict[header] = defaultdict(dict)
+                    exons_dict[header][exon_number] = exon
+            # just print genes that don't have intron
+            #else:
+                #print the FASTA header
+                print(">" + species + "_" + gene_name)
+                # extract the sequence
+                print(get_seq(int(start)-1, int(end), strand))
+        #skip all other feature type
+        else :
+            continue
+
+# loop over expns dictionary(h=header, j=list of exons)
+for i, j in exons_dict.items():
+    #print the header
+    print(">" + i)
+    # loop over the dictionary of exons, and print them
+    for k in sorted(j):
+        # print the individual exon
+        print(j[k], end='')
+    print()
+            
+
 '''
         # check in reverse complement
         if strand == '-':
